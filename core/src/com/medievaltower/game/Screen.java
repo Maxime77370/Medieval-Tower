d95d9bdd072f4d997c487c7aba7c37a6b5d11132
Carpentier Maxime <maxime.carpentier@epitech.eu>
2023-11-08 17:10:15 +0100

    start Screen part

--- core/src/com/medievaltower/game/Screen.java
+++ core/src/com/medievaltower/game/Screen.java
@@ -1,4 +1,41 @@
 package com.medievaltower.game;
 
-public class Screen {
+public abstract class Screen {
+
+    private int width;
+    private int height;
+    private String type;
+
+    public Screen(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean loadParameters(){
+        return true;
+    }
+
+    public void setScreenType(String type){
+        this.type = type;
+    }
+
+    public String getScreenType(){
+        return type;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void setHeight(int height){
+        this.height = height;
+    }
 }
