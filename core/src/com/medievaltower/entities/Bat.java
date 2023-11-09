e07bc719965e356e395236ac23598db07a9c9d08
OlivierPAKOSZ <olivier.pakosz@epitech.eu>
2023-11-09 14:30:35 +0100

    Ajout des Classes Zombie, Archer et Bat

---
+++ core/src/com/medievaltower/entities/Bat.java
@@ -0,0 +1,11 @@
+package com.medievaltower.entities;
+
+public class Bat extends Monstre{
+    public Bat(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void move() {
+    }
+}
