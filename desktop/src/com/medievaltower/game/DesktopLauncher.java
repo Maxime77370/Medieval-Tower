b2bf705768bdf3f0bf3e42b11a0190eb6402f411
Carpentier Maxime <maxime.carpentier@epitech.eu>
2023-11-09 10:31:25 +0100

    screen size modification

--- desktop/src/com/medievaltower/game/DesktopLauncher.java
+++ desktop/src/com/medievaltower/game/DesktopLauncher.java
@@ -1,15 +1,35 @@
 package com.medievaltower.game;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.medievaltower.game.MedievalTower;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+
+	private static int width;
+	private static int height;
+	private static Graphics.DisplayMode fullscreen;
 	public static void main (String[] arg) {
+
+		loadParameters();
+
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+
+		config.setTitle("MedievalTower");
+		config.setWindowedMode(width, height);
+
 		config.setForegroundFPS(60);
 		config.setTitle("MedievalTower");
 		new Lwjgl3Application(new MedievalTower(), config);
+	}
+
+	public static boolean loadParameters(){
+		width = 800;
+		height = 600;
+		fullscreen = ;
+
+		return true;
 	}
 }
