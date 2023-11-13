942adf4f1988bbb16d92a21b817cb54abf90842e
OlivierPAKOSZ <olivier.pakosz@epitech.eu>
2023-11-13 14:49:06 +0100

    Ajout d'une flèche avec gravité tirée toutes les 5s

--- core/src/com/medievaltower/entities/monster/Archer.java
+++ core/src/com/medievaltower/entities/monster/Archer.java
@@ -1,6 +1,12 @@
 package com.medievaltower.entities.monster;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.medievaltower.core.Entity;
+import com.medievaltower.core.EntityManager;
+import com.medievaltower.entities.Personnage;
+
+import java.util.Date;
 
 /**
  * Class Archer
@@ -15,13 +21,21 @@ import com.badlogic.gdx.graphics.Texture;
  */
 public class Archer extends Monstre {
 
+    private float timeSinceLastShot = 0;
+    private Arrow currentArrow;
+    private static final float SHOOT_INTERVAL = 5; // Intervalle de tir en secondes
+    private EntityManager entityManager;
+    private Date timer;
+
+
     /**
      * Archer constructor
      * @param x : the x position of the monster
      * @param y : the y position of the monster
      */
-    public Archer(int x, int y) {
+    public Archer(int x, int y){
         super(x, y, 50, 50, new Texture("paix.jpg"));
+        this.entityManager = EntityManager.getInstance();
     }
 
     /**
@@ -50,23 +64,36 @@ public class Archer extends Monstre {
      */
     @Override
     public void attack() {
-
+        currentArrow = new Arrow((int) getX(), (int) getY());
+        entityManager.newEntity(currentArrow);
+        timer = new Date();
     }
 
-    /**
-     * Receive damage
-     * @param damage : the damage received
-     */
     @Override
     public void receiveDamage(int damage) {
 
     }
+// Je regarde si ça fait 5 secondes que j'ai tiré (objet Date).
+    // Si j'ai pas tiré, j'appelle la fonction attaque qui crée une flèche.
 
-    /**
-     * Update the archer
-     */
+    public Arrow getCurrentArrow() {
+        return currentArrow;
+    }
+
     @Override
     public void update() {
+        super.update();
         this.move();
+
+        // Vérifie si la flèche actuelle est nulle
+        if (currentArrow == null) {
+            attack();
+        }
+        else if (new Date().getTime() - timer.getTime() > SHOOT_INTERVAL * 1000) {
+            entityManager.removeEntity(currentArrow);
+            System.out.println("delete");
+            attack();
+        }
+        // Tir la flèche si la condition est remplie
     }
 }
