0a83d5eb33443711a160de96b3a4a36dd14c844a
OlivierPAKOSZ <olivier.pakosz@epitech.eu>
2023-11-14 11:51:07 +0100

    ajout fichiers .tsx pour la première map

---
+++ assets/Maps/tiled/Rocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="Props-Rocks" tilewidth="16" tileheight="16" tilecount="378" columns="18">
+ <image source="../../Texture/Legacy-Fantasy - High Forest 2.3/Assets/Props-Rocks.png" width="288" height="336"/>
+</tileset>
