2f8afde11dc65bae8c6f3bbcf90ca905ce9b6e2e
OlivierPAKOSZ <olivier.pakosz@epitech.eu>
2023-11-21 10:16:59 +0100

    Ajout de la map 4. Correction bug collision map 3 (grotte). Ajout de plusieurs monstres. Ajout spawn Potion sur map 2 et 3. Ajout end_game à la fin du niveau

---
+++ assets/Maps/tiled/castle_tileset_part2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="castle_tileset_part2" tilewidth="16" tileheight="16" tilecount="1344" columns="32">
+ <image source="../../../../../../Downloads/castle_tileset_part2.png" width="512" height="672"/>
+</tileset>
---
+++ assets/Maps/tiled/chateau_agrandi_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="chateau_agrandi_3" tilewidth="16" tileheight="16" tilecount="3072" columns="64">
+ <image source="../../../../../../Downloads/chateau_agrandi_3.png" width="1024" height="768"/>
+</tileset>
