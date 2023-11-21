8286cf90f6075383d11388a8b2cfadb8a3aa667f
OlivierPAKOSZ <olivier.pakosz@epitech.eu>
2023-11-21 15:03:07 +0100

    ajout map4

--- assets/Maps/tiled/castle_tileset_part1.tsx
+++ assets/Maps/tiled/castle_tileset_part1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="git" name="castle_tileset_part1" tilewidth="16" tileheight="16" tilecount="2688" columns="32">
- <image source="../../../../../../Downloads/castle_tileset_part1.png" width="512" height="1344"/>
+ <image source="../../Texture/Assets_map4/castle_tileset_part1.png" width="512" height="1344"/>
 </tileset>
